f22150945875da8dfc4ce2c75b3e86cac6679ca1
Frank <frank@iMac3.local>
2018-11-06 11:50:07 -0500

    finished

--- assingment_unit_seven.py
+++ assingment_unit_seven.py
@@ -1,15 +1,31 @@
 characters = "abcdefghijklmnopqrstuvwxyz"
-encode_word = input("what word you want to encode?")
-decode_word = input("what word yo want to decode?")
 
 
 def main():
     x = int(input("what number do you want to put in?"))
     characters1 = characters[0:x]
-    characters2 = characters[x:26]
+    characters2 = characters[x:]
     characters3 = characters2 + characters1
-    print(characters3)
-    characters.index(encode_word)
+    a = input("which function do you want to proceed? Put in 'e' for encode, 'd' for decode, 'q' for quit")
+    if a is "e":
+        encode_word = input("what word you want to encode?")
+        new_string = ""
+        for character in encode_word:
+            code = characters.index(character)
+            new_character = characters3[code]
+            new_string = new_string + new_character
+        print(new_string)
+    elif a is "d":
+        decode_word = input("what word yo want to decode?")
+        new_string = ""
+        for character in decode_word:
+            code = characters3.index(character)
+            new_character = characters[code]
+            new_string = new_string + new_character
+        print(new_string)
+    else:
+        print("thank you for using!")
 
 
 main()
+
